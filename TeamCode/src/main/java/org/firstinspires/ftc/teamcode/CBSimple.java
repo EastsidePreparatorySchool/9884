4261690f50f5b0a2a754def1c73c8690719215c8
Gunnar Mein <gmein@eastsideprep.org>
2018-02-02 17:02:25 -0800

    read below
    - cheerbot
    - murderbot
    - upgrade to platform3.6

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CBSimple.java
@@ -0,0 +1,144 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Cheerbotbot: Telop Simple", group = "Cheerbot")
+
+public class CBSimple extends LinearOpMode {
+
+    /* Declare OpMode members. */
+    HardwareCheerbot robot = new HardwareCheerbot();
+
+    @Override
+    public void runOpMode() {
+        double left;
+        double right;
+        double up;
+        double down;
+        double position;
+
+        /* Initialize the hardware variables.
+         * The init() method of the hardware class does all the work here
+         */
+        robot.init(hardwareMap);
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Say", "Hello Mr. Mein");
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+//            position =-1;
+//
+//            left = gamepad1.left_stick_y;
+//            right = gamepad1.right_stick_y;
+//            if(gamepad1.right_bumper){
+//                robot.leftFrontMotor.setPower(0.4*left);
+//                robot.rightFrontMotor.setPower(0.4*right);
+//                robot.leftBackMotor.setPower(0.4*left);
+//                robot.rightBackMotor.setPower(0.4*right);
+//            }  else {
+//                robot.leftFrontMotor.setPower(left);
+//                robot.rightFrontMotor.setPower(right);
+//                robot.leftBackMotor.setPower(left);
+//                robot.rightBackMotor.setPower(right);
+            robot.leftFrontMotor.setPower(1.0);
+            robot.rightFrontMotor.setPower(1.0);
+            robot.leftBackMotor.setPower(1.0);
+            robot.rightBackMotor.setPower(1.0);
+        }
+
+
+//            // triggers lifters
+//            if(gamepad1.left_trigger >0) { //&& (robot.lifter.getCurrentPosition() <-48)){
+//                robot.lifter.setPower(1*gamepad1.left_trigger);
+//            } else if (gamepad1.right_trigger>0) { //&& (robot.lifter.getCurrentPosition() > -12876.1)){
+//                robot.lifter.setPower(-1*gamepad1.right_trigger);
+//            } else {
+//                robot.lifter.setPower(0.0);
+//
+//            }
+//            //TODO: Servos
+//            // Use gamepad Y & A raise and lower the arm
+//            if (gamepad1.a) {
+//                whackerPosition = 0;
+//            }
+//            else if (gamepad1.y) {
+//                whackerPosition = 0.4;
+//            }
+//            if(gamepad1.x){
+//                leftArmPosition -= 0.1;
+//                rightArmPosition +=0.1;
+//            } else if (gamepad1.b){
+//                leftArmPosition += 0.1;
+//                rightArmPosition -=0.1;
+//            }
+//            if(gamepad1.left_bumper){
+//                position = robot.lifter.getCurrentPosition();
+//            }
+//
+////
+////            // Use gamepad X & B to open and close the claw
+////            if (gamepad91.x)
+////                clawPosition += CLAW_SPEED;
+////            else if (gamepad1.b)
+////                clawPosition -= CLAW_SPEED;
+////
+////            // Move both servos to new position.
+//            // at 0 right is all the way in and left is all the way out
+//            whackerPosition  = Range.clip(whackerPosition, 0.0, 0.6);
+//            rightArmPosition = Range.clip(rightArmPosition, 0.3, 0.8);
+//            leftArmPosition = Range.clip(leftArmPosition, 0.3,0.8);
+//            robot.whacker.setPosition(whackerPosition);
+//            robot.leftArm.setPosition(leftArmPosition);
+//            robot.rightArm.setPosition(rightArmPosition);
+////            robot.arm.setPosition(armPosition);
+////            clawPosition = Range.clip(clawPosition, robot.CLAW_MIN_RANGE, robot.CLAW_MAX_RANGE);
+////            robot.claw.setPosition(clawPosition);
+//
+
+
+        // Send telemetry message to signify robot running;
+//            telemetry.addData("position","%.2f", position);
+//            telemetry.addData("left",  "%.2f", left);
+//            telemetry.addData("right", "%.2f", right);
+//            telemetry.update();
+
+        // Pause for 40 mS each cycle = update 25 times a second.
+        sleep(40);
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMurderbot.java
@@ -0,0 +1,84 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * Hardware definitions for Murderbot
+ */
+public class HardwareMurderbot {
+    /* Public OpMode members. */
+    public DcMotor leftFrontMotor = null;
+    public DcMotor rightFrontMotor = null;
+    public DcMotor leftBackMotor = null;
+    public DcMotor rightBackMotor = null;
+
+
+    /* local OpMode members. */
+    HardwareMap hwMap = null;
+    private ElapsedTime period = new ElapsedTime();
+
+    /* Constructor */
+    public HardwareMurderbot() {
+
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        // Save reference to Hardware map
+        hwMap = ahwMap;
+        // hey bob
+
+        // Define and Initialize Motors
+        leftFrontMotor = hwMap.dcMotor.get("lfm");
+        rightFrontMotor = hwMap.dcMotor.get("rfm");
+        leftBackMotor = hwMap.dcMotor.get("lbm");
+        rightBackMotor = hwMap.dcMotor.get("rbm");
+
+        leftFrontMotor.setDirection(DcMotor.Direction.REVERSE);
+        rightFrontMotor.setDirection(DcMotor.Direction.REVERSE);
+        leftBackMotor.setDirection(DcMotor.Direction.REVERSE);
+        rightBackMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        // Set all motors to run without encoders.
+        // May want to use RUN_USING_ENCODERS if encoders are installed.
+        leftFrontMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightFrontMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftBackMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightBackMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        leftFrontMotor.setPower(0.0);
+        rightFrontMotor.setPower(0.0);
+        leftBackMotor.setPower(0.0);
+        rightBackMotor.setPower(0.0);
+    }
+
+    /***
+     *
+     * waitForTick implements a periodic delay. However, this acts like a metronome with a regular
+     * periodic tick.  This is used to compensate for varying processing times for each cycle.
+     * The function looks at the elapsed cycle time, and sleeps for the remaining time interval.
+     *
+     * @param periodMs  Length of wait cycle in mSec.
+     */
+
+    public void waitForTick(long periodMs) {
+
+        long remaining = periodMs - (long) period.milliseconds();
+
+        // sleep for the remaining portion of the regular cycle period.
+        if (remaining > 0) {
+            try {
+                Thread.sleep(remaining);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+
+        // Reset the cycle clock for the next pass.
+        period.reset();
+    }
+}
+
